10b42a940551c61dcbea12bbbaab6e173432156b
Gordon Dickens <gordon@gordondickens.com>
2011-09-22 22:38:28 -0400

    updated Roo

---
+++ demos/simaildemo/src/main/java/service/RecipientService.java
@@ -0,0 +1,7 @@
+package service;
+
+import org.springframework.roo.addon.layers.service.RooService;
+
+@RooService(domainTypes = { com.gordondickens.simail.entity.Recipient.class })
+public interface RecipientService {
+}
